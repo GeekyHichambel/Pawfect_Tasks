5004232b4c37929e0b030b96ecc78a8fe4df0f6b
Hichambel <parthvij23@gmail.com>
2024-01-16 23:05:31 +0530

    dev phase

--- lib/pages/ProfilePane.dart
+++ lib/pages/ProfilePane.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hit_me_up/Components/AppTheme.dart';
+import 'package:hit_me_up/Components/CustomBox.dart';
 
 class ProfilePane extends StatefulWidget{
   const ProfilePane({Key? key}) : super(key: key);
@@ -8,22 +9,129 @@ class ProfilePane extends StatefulWidget{
 }
 
 class _ProfilePaneState extends State<ProfilePane>{
+  bool imageUp = false;
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppTheme.colors.gloryBlack,
-      body: Column(
-        children: [
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                borderRadius: BorderRadius.circular(16.0),
+      body: Padding(
+          padding: const EdgeInsetsDirectional.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(padding: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 0.0),
+                child: IconButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    }, icon: Icon(Icons.arrow_back_ios_new_rounded,
+                  size: 30.0,
+                  color: AppTheme.colors.blissCream,)),
               ),
-            ),
-          )
-        ],
-      )
+              Expanded(child:
+              SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30.0,),
+                    Center(
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                            color: AppTheme.colors.complimentaryBlack,
+                            shape: BoxShape.circle,
+                            boxShadow: [BoxShadow(
+                              color: AppTheme.colors.blissCream,
+                              offset: const Offset(0, 0),
+                              blurRadius: 30.0,
+                            ),
+                            ]
+                        ),
+                        child: Center(
+                          child: imageUp? null :
+                          Text('F',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Onset',
+                              fontSize: 80,
+                              color: AppTheme.colors.blissCream,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),),
+                    const SizedBox(height: 10.0,),
+                    Padding(padding: const EdgeInsetsDirectional.all(16.0),
+                      child: ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: [
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                          const SizedBox(height: 16.0),
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                          const SizedBox(height: 16.0),
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                          const SizedBox(height: 16.0),
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                          const SizedBox(height: 16.0),
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                          const SizedBox(height: 16.0),
+                          CustomBox(color: AppTheme.colors.complimentaryBlack,
+                              shadow: Colors.transparent,
+                              height: 60,
+                              child: const Row(
+                                children: [
+
+                                ],
+                              )),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
